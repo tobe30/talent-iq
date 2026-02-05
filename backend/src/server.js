f060e9d0fe20fdf4e8669082ec6457b46c522875
Tobe Marizu <tobemarizu@gmail.com>
2026-02-05 13:04:22 +0100

    import functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
+import { serve } from 'inngest/express';
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
-import {inngest} from "./lib/inngest.js";
+import {inngest, functions} from "./lib/inngest.js";
 const app = express();
 
 

3f7abfd5bc492df91dfdefaba209fcd6b3ecd6c8
Tobe Marizu <tobemarizu@gmail.com>
2026-02-06 10:44:48 +0100

    auth & stream sync

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import  { connectDB } from "./db.js";
 import User from "../models/User.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "talent-iq"});
 
@@ -20,6 +21,12 @@ const syncUser = inngest.createFunction(
         }
 
        await User.create(newUser);
+
+            await upsertStreamUser({
+            id: newUser.clerkId.toString(),
+            name: newUser.name,
+            image: newUser.profileImage
+          })
     }
 )
 
@@ -34,7 +41,7 @@ const deleteUserFromDB = inngest.createFunction(
         const {id} = event.data;
           await User.deleteOne({clerkId: id});
 
-          //todo
+          await deleteStreamUser(id.toString());
     }
 )
 export const functions = [syncUser, deleteUserFromDB];

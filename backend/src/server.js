38667d97cacb2dc994ef4deae235c8cc98b49d81
Tobe Marizu <tobemarizu@gmail.com>
2026-02-06 12:34:18 +0100

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,11 @@ import express from 'express';
 import path from 'path';
 import cors from 'cors';
 import { serve } from 'inngest/express';
+import { clerkMiddleware } from '@clerk/express'
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
 import {inngest, functions} from "./lib/inngest.js";
+import chatRoutes from './routes/chatRoutes.js';
 const app = express();
 
 
@@ -14,18 +16,19 @@ const __dirname = path.resolve();
 app.use(express.json());
 //credentials:true meaning??=> server allows a browser to include cookies on request
 app.use(cors({origin: ENV.CLIENT_URL, credentials:true}));
-
+app.use(clerkMiddleware()); //this adds auth field to req object: req.auth()
 app.use("/api/inngest", serve({client:inngest, functions}));
-
+app.use("/api/chat", chatRoutes); //chat routes
 
 app.get("/health", (req, res) => {
     res.status(200).json({msg:"api is up and running"})
 });
 
-app.get("/books", (req, res) => {
-    res.status(200).json({msg:"this is the books endpoint"})
-});
 
+//when you pass an array of middleware to Express, it automatically, flattens and executes them sequentially. one by one.
+// app.get("/video-calls", protectRoute, (req, res) => {
+//     res.status(200).json({msg:"this is a protected video calls endpoint"})
+// });
 
 //make ready for deployment
 if(ENV.NODE_ENV === "production") {
@@ -51,4 +54,4 @@ const startServer = async () => {
     }
 }
 
-startServer();
+startServer(); 

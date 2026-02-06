41d6e151e355dca06d01038eeece69d6e0987105
Tobe Marizu <tobemarizu@gmail.com>
2026-02-06 12:38:22 +0100

    minor typo fix

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,12 +7,12 @@ export const protectRoute = [
         try {
             const clerkId = req.auth().userId; 
             
-            if(!clerkId) return res.status(401).json({msg:"Unauthorized - invalid token"});
+            if(!clerkId) return res.status(401).json({message:"Unauthorized - invalid token"});
 
             //find user in db by clerk ID
             const user = await User.findOne({clerkId});
 
-            if(!user) return res.status(404).json({msg:"User not found"});
+            if(!user) return res.status(404).json({message:"User not found"});
 
             // attach user to req
             req.user = user; 
@@ -20,7 +20,7 @@ export const protectRoute = [
             next();
         } catch (error) {
             console.error("Error in protectRoute middleware:", error);
-            res.status(500).json({msg:"Internal server error"});
+            res.status(500).json({message:"Internal server error"});
         }
     }
 ]

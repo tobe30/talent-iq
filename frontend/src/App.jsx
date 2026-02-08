9ad717715a8984f670ea176d71dcee502e13c4e3
Tobe Marizu <tobemarizu@gmail.com>
2026-02-08 23:32:51 +0100

    landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
 
 import { SignInButton, useUser } from '@clerk/clerk-react'
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage'
 import { Toaster } from 'react-hot-toast'
+import DashboardPage from './pages/DashboardPage'
 
 function App() {
 
-  const {isSignedIn} = useUser()
+  const {isSignedIn,isLoaded} = useUser()
+
+  //this will get rid of the flickering effect
+  if(!isLoaded) return null; // return nothing
   return (
     <>
     <Routes>
-    <Route path='/' element={<HomePage/>}/>
+    <Route path='/' element={!isSignedIn ? <HomePage/> : <Navigate to={"/dashboard"}/>}/>
+    <Route path='/dashboard' element={isSignedIn ? <DashboardPage/> : <Navigate to={"/"}/>}/>
+
     <Route path='/problems' element={isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}/>
 
     </Routes>

283193f5528e7dc0f2f8706593fd348a2bfd71dc
Tobe Marizu <tobemarizu@gmail.com>
2026-02-12 01:00:59 +0100

    session page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import ProblemsPage from './pages/ProblemsPage'
 import { Toaster } from 'react-hot-toast'
 import DashboardPage from './pages/DashboardPage'
 import ProblemPage from './pages/ProblemPage'
+import SessionPage from './pages/SessionPage'
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
 
     <Route path='/problems' element={isSignedIn ? <ProblemsPage/> : <Navigate to={"/"}/>}/>
     <Route path='/problem/:id' element={isSignedIn ? <ProblemPage/> : <Navigate to={"/"}/>}/>
+    <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
 
 
     </Routes>
